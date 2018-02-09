fb4f29836f2ed29741c8469214db5051a1b284e9
Tariq Ettaji <tettaji@xebia.com>
2018-02-09 10:23:32 +0100

    Added Brigade files

--- tests/app_tests.py
+++ tests/app_tests.py
@@ -3,11 +3,17 @@ import uuid
 
 import app
 
+# Helper
+
+def bytes_to_str(b):
+    return ''.join(chr(x) for x in (b))
+    
 class AppTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.app = app.app
+        self.client = app.app.test_client()
 
-    def test_uuid_generated(self):
-        resp = self.app.test_client().get('/')
-        assert uuid.UUID(resp.data)
+
+    def test_brigade_ci_meetup(self):
+        resp = self.client.get('/')
+        assert uuid.UUID(bytes_to_str(resp.data))
